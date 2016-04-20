a4168d7f0e2019bd39226abad842ad9f8e1be56c
Matt Heard <matt.heard@powershop.co.nz>
2016-04-21 10:44:45 +1200

    Remove ability to edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def edit
-  end
-
   def create
     @user = User.new(user_params)
     if ValidateHundredWordLength.new(description).call && @user.save
