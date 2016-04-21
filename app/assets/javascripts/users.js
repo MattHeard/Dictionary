d5ba85fc7d22c691e62cb8839fdbbbe038afb3c8
Matt Heard <matt.heard@powershop.co.nz>
2016-04-21 20:31:25 +1200

    Add client-side word count for textareas

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,15 @@
+function countNonEmptyStrings(list) {
+  var count = 0;
+  for (var i = 0; i < list.length; i++) {
+    if (list[i]) { count++; }
+  }
+  return count;
+}
+
+function updateWordCount() {
+  textArea = document.getElementById("user_description") || document.getElementById("word_definition");
+  words = textArea.value.trim().split(" ");
+  length = countNonEmptyStrings(words);
+  wordCount = document.getElementById("word_count");
+  wordCount.innerHTML = length + "/100";
+}
