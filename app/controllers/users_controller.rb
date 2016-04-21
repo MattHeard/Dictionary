2ead95cadc06d54aacb95824e04f4e472daea770
Matt Heard <matt.heard@powershop.co.nz>
2016-04-22 10:32:38 +1200

    Do not highlight common words

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  COMMON_WORDS = %w{
+    the of and a to in is you that it he was for on are as with his they I at be
+    this have from or one had by word but not what all were we when your can
+    said there use an each which she do how their if will up other about out
+    many then them these so some her would make like him into time has look two
+    more write go see number no way could people my than first water been call
+    who oil its now find long down day did get come made may part
+  }
+
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -52,12 +61,16 @@ class UsersController < ApplicationController
   def set_frequencies
     descriptions = User.pluck(:description)
     words = descriptions.join(" ").split.map { |word| SimplifiedWord.new(word).call }
-    pp words
+    words -= common_words
     @frequencies = words.inject({}) do |frequencies, word|
       count = words.count { |w| w == word }
       frequencies[word] = count if count > 1
       frequencies
     end
     @frequencies.default = 0
+  end
+
+  def common_words
+    COMMON_WORDS
   end
 end
--- app/queries/simplified_word.rb
+++ app/queries/simplified_word.rb
@@ -1,10 +1,20 @@
 class SimplifiedWord
+  WHITELIST = %w{
+    I is was his Wikipedia US us politics ethics overseas as 80s Timberlake's
+    has
+  }
+
   def initialize(word)
     @word = word
   end
 
   def call
-    word.downcase.gsub(matcher, "").singularize
+    word.gsub!(matcher, "")
+    if whitelist.include?(word)
+      word
+    else
+      word.downcase.singularize
+    end
   end
 
   private
@@ -13,5 +23,9 @@ class SimplifiedWord
 
   def matcher
     /^[^[:word:]]*|[^[:word:]]*$/
+  end
+
+  def whitelist
+    WHITELIST
   end
 end
