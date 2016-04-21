4ee6288912a46329a97ff6647b97ee7e3dd9aae3
Matt Heard <matt.heard@powershop.co.nz>
2016-04-21 13:41:52 +1200

    Create new word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,16 +2,39 @@ class WordsController < ApplicationController
   def new
     set_user
     @word = @user.words.new
-    @word.word = word_params[:word]
+    @word.word = word_params[:new_word]
+  end
+
+  def create
+    set_user
+    if ValidateHundredWordLength.new(definition).call || true
+      word = @user.words.build(word_params[:word])
+      if word.save
+        redirect_to user_word_url(@user, word)
+      end
+    end
+  end
+
+  def show
+    set_user
+    set_word
   end
 
   private
 
   def set_user
-    @user = User.find(word_params[:user_id])
+    @user = User.find(params[:user_id])
+  end
+
+  def set_word
+    @word = @user.words.find(params[:id])
   end
 
   def word_params
-    params.permit(:user_id, :word)
+    params.permit(:new_word, :user_id, word: [ :word, :definition ])
+  end
+
+  def definition
+    params[:word][:definition]
   end
 end
